2e6dbcc1319a0b2f6882fb81e590864343256bf8
Adelshkeir <adelshkeir12@gmail.com>
2024-01-22 16:32:35 +0200

    finished the design of the admin reviews page

---
+++ Rayn-look/src/Admin/adminreviews/adminreviewcard.jsx
@@ -0,0 +1,22 @@
+import { FaCircleUser } from "react-icons/fa6";
+ const Adminreviewcard = () =>{
+    return(
+        <div className="Admin-review-card">
+            <div className="Admin-review-card-details">
+
+<div className="Admin-review-card-details-first">
+   <FaCircleUser className="reviews-image"/>
+<h4>Name</h4>
+
+</div>
+<p>this is a very nice product</p>
+
+
+</div>
+
+
+        </div>
+    )
+ }
+
+ export default Adminreviewcard
--- Rayn-look/src/Admin/adminreviews/adminreviews.jsx
+++ Rayn-look/src/Admin/adminreviews/adminreviews.jsx
@@ -1,7 +1,31 @@
+import Adminreviewcard from "./adminreviewcard"
+import TextField from "@mui/material/TextField";
+import "./adminreviews.css"
 const Adminreviews = () => {
     return(
         <div className="Admin-display-container">
-            Reviews
+              <div className="admin-reviews-search-bar">        
+        <TextField 
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+ </div>
+           <div className="admin-reviews-container">
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            <Adminreviewcard />
+            </div>
         </div>
     )
 }
